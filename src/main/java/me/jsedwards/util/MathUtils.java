42f27586766e9c487dee778af91923fe6356c094
Joseph Edwards <joseph.edwards@learnatwren.org>
2024-01-01 16:44:59 +0000

    Added rounding utility method

--- src/main/java/me/jsedwards/util/MathUtils.java
+++ src/main/java/me/jsedwards/util/MathUtils.java
@@ -7,4 +7,8 @@ public class MathUtils {
     public static double scale(double srcStart, double srcEnd, double destStart, double destEnd, double src) {
         return destStart + (src - srcStart) / (srcEnd - srcStart) * (destEnd - destStart);
     }
+
+    public static double roundToNearestMultiple(double n, double round) {
+        return Math.round(n / round) * round;
+    }
 }

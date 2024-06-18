b879a2953ec42fe350bd2c41aceecbcd1dc32475
Joseph Edwards <83555891+Contrabass26@users.noreply.github.com>
2024-06-18 20:29:27 +0100

    Update Fabric loader version in test

--- src/test/java/MinecraftWrapperTest.java
+++ src/test/java/MinecraftWrapperTest.java
@@ -11,7 +11,7 @@ public class MinecraftWrapperTest {
 
     @Test
     public void fabricVersions() {
-        Assertions.assertEquals("0.15.6", ModLoader.FABRIC_LOADER_VERSION, "Wrong Fabric loader version");
+        Assertions.assertEquals("0.15.11", ModLoader.FABRIC_LOADER_VERSION, "Wrong Fabric loader version");
         Assertions.assertEquals("1.0.0", ModLoader.FABRIC_INSTALLER_VERSION, "Wrong Fabric installer version");
     }
 

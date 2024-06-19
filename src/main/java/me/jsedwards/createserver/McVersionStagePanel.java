3eca2c0c2bce7662b88925c621082a5d809f5c6c
Joseph Edwards <joseph.edwards@learnatwren.org>
2024-06-19 13:15:26 +0100

    Auto-detct all Minecraft releases

--- src/main/java/me/jsedwards/createserver/McVersionStagePanel.java
+++ src/main/java/me/jsedwards/createserver/McVersionStagePanel.java
@@ -1,13 +1,34 @@
 package me.jsedwards.createserver;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import me.jsedwards.Main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class McVersionStagePanel extends ValidatedStage {
 
-    private static final String[] VERSIONS = {"1.8.9", "1.9", "1.9.4", "1.10", "1.10.2", "1.11", "1.11.2", "1.12", "1.12.1", "1.12.2", "1.13.2", "1.14", "1.14.1", "1.14.2", "1.14.3", "1.14.4", "1.15", "1.15.1", "1.15.2", "1.16", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.16.5", "1.17", "1.17.1", "1.18", "1.18.1", "1.18.2", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4"};
+    private static final List<String> VERSIONS;
+    static {
+        VERSIONS = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            ArrayNode versions = (ArrayNode) mapper.readTree(new URL("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json")).get("versions");
+            for (JsonNode version : versions) {
+                if (version.get("type").textValue().equals("release")) {
+                    VERSIONS.add(version.get("id").textValue());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private final JComboBox<String> comboBox;
 
@@ -20,7 +41,7 @@ public class McVersionStagePanel extends ValidatedStage {
         label.setFont(Main.MAIN_FONT.deriveFont(18f));
         this.add(label, new GridBagConstraints(1, 1, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
         // Combo box
-        comboBox = new JComboBox<>(VERSIONS);
+        comboBox = new JComboBox<>(VERSIONS.toArray(new String[0]));
         this.add(comboBox, new GridBagConstraints(2, 1, 1, 1, 1, 0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(0, 20, 0, 0), 0, 0));
     }
 

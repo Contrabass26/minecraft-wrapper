3ccf802d7a45ed414cfe0eec069f58909a8b1faa
Joseph Edwards <joseph.edwards@learnatwren.org>
2024-06-19 15:09:50 +0100

    Started better Spigot property descriptions

--- src/main/java/me/jsedwards/configserver/SpigotConfigManager.java
+++ src/main/java/me/jsedwards/configserver/SpigotConfigManager.java
@@ -2,18 +2,13 @@ package me.jsedwards.configserver;
 
 import me.jsedwards.dashboard.Server;
 import org.apache.commons.lang3.StringUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 import java.util.*;
 import java.util.function.Function;
-import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class SpigotConfigManager extends YamlConfigManager {
 
@@ -24,43 +19,22 @@ public class SpigotConfigManager extends YamlConfigManager {
     private static final Map<String, Boolean> KEYS_ENABLED = new HashMap<>();
 
     static {
-        // Property descriptions
-        Pattern pattern = Pattern.compile("Default: ((?:.(?!Type:))*) Type: ((?:.(?!Description:))*) Description: ((?:.(?!Default:))*)");
-        try {
-            Document document = Jsoup.connect("https://www.spigotmc.org/wiki/spigot-configuration/").userAgent("Mozilla").get();
-            List<Node> children = document.select(".page-content").get(0).childNodes();
-            for (int i = 0; i < children.size(); i++) {
-                Node node = children.get(i);
-                if (node instanceof Element child && child.is("span")) {
-                    String key = child.text();
-                    if (PROPERTY_DESCRIPTIONS.containsKey(key)) {
-                        PROPERTY_DESCRIPTIONS.put(key, "Not found");
-                    } else {
-                        StringBuilder text = new StringBuilder();
-                        for (int j = i + 1; j < children.size(); j++) {
-                            Node candidate = children.get(j);
-                            if (candidate instanceof Element element) {
-                                if (element.is("style")) continue;
-                                if (element.is("span")) {
-                                    Optional<MatchResult> matcher = pattern.matcher(text).results().findFirst();
-                                    if (matcher.isPresent()) {
-                                        MatchResult matchResult = matcher.get();
-                                        PROPERTY_DEFAULTS.put(key, matchResult.group(1));
-                                        PROPERTY_DATA_TYPES.put(key, matchResult.group(2));
-                                        PROPERTY_DESCRIPTIONS.put(key, matchResult.group(3));
-                                    }
-                                    break;
-                                }
-                                text.append(element.text());
-                            } else if (candidate instanceof TextNode textNode) {
-                                text.append(textNode.text());
-                            }
-                        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL("https://docs.papermc.io/assets/js/281a9c5e.2220deb7.js").openStream()))) {
+            String content = reader.lines().collect(Collectors.joining());
+            Pattern pattern = Pattern.compile("const i='(advancements.+[^\\\\])'");
+            pattern.matcher(content).results().findFirst().ifPresentOrElse(matchResult -> {
+                String[] lines = matchResult.group(1).split("\n");
+                Stack<String> currentPath = new Stack<>();
+                for (int i = 0; i < lines.length; i++) {
+                    String line = lines[i];
+                    if (line.startsWith("default:")) {
+                        // This line and the next form a description of the current path
+                        String defaultValue = line.
                     }
                 }
-            }
-        } catch (IOException e) {
-            LOGGER.error("Failed to get Spigot property descriptions", e);
+            }, () -> {throw new IllegalStateException("No descriptions found in https://docs.papermc.io/assets/js/281a9c5e.2220deb7.js");});
+        } catch (IOException | IllegalStateException e) {
+            LOGGER.warn("Failed to get property descriptions for spigot.yml", e);
         }
         OPTIMISATION_FUNCTIONS.put("world-settings/default/view-distance", ConfigManager.VIEW_DISTANCE_OPTIMISATION);
         OPTIMISATION_FUNCTIONS.put("world-settings/default/simulation-distance", ConfigManager.SIMULATION_DISTANCE_OPTIMISATION);
